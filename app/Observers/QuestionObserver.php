ff72ac8db8caa9206bafefc111d8f4f218083fe2
MBABAZI Jacques <mbabazijacques@gmail.com>
2021-11-10 10:55:06 +0200

    Add notification of Question

--- app/Observers/QuestionObserver.php
+++ app/Observers/QuestionObserver.php
@@ -1,8 +1,16 @@
 <?php
 
 namespace App\Observers;
+use App\Models\Question;
+use App\Models\User;
+use App\Notifications\QuestionNotification;
 
 class QuestionObserver
 {
-    //
+    public function created(Question $question)
+    {
+        User::with('mentor')->where('mentor_id', $question->mentor->id)->get()->each(function (User $user) use ($question) {
+            $user->notify(new QuestionNotification($question));
+        });
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Question;
+use App\Notifications\QuestionNotification;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Question::observe(QuestionNotification::class);
     }
 }
