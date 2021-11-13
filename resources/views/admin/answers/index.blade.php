f766bd1e036be14368a373cde79bb2d54d5a6d9e
MBABAZI Jacques <mbabazijacques@gmail.com>
2021-11-13 13:20:56 +0200

    Update index.blade.php

--- resources/views/admin/answers/index.blade.php
+++ resources/views/admin/answers/index.blade.php
@@ -4,7 +4,7 @@
     <div style="margin-bottom: 10px;" class="row">
         <div class="col-lg-12">
             <a class="btn btn-success" href="{{ route('admin.answers.create') }}">
-                } {{ trans('cruds.answer.title_singular') }}
+                 {{ trans('cruds.answer.title_singular') }}
             </a>
         </div>
     </div>
